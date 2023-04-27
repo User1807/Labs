1bf1645a0d4850786756c815fe2292b969b4b257
User1807 <116075592+User1807@users.noreply.github.com>
2023-04-27 19:26:11 +0500

    Add files via upload

--- OOP/Lab7/Lab7_main.cpp
+++ OOP/Lab7/Lab7_main.cpp
@@ -1,4 +1,4 @@
-﻿#include "Mnojestvo.h"
+﻿#include "Vector.h"
 #include "Money.h"
 #include <iostream>
 using namespace std;
@@ -6,11 +6,11 @@ void main()
 {
 	cout << "Enter set of 5 values: " << endl;
 	//инициализация, ввод и вывод значений вектора
-	Mnojestvo<long>A(5, 0);
+	Vector<long>A(5, 0);
 	cin >> A;
 	cout << A << endl;
 	//инициализация и вывод значений вектора
-	Mnojestvo <long>B(10, 1);
+	Vector <long>B(10, 1);
 	cout << B << endl;
 	//операция присваивания
 	B = A;
@@ -20,7 +20,7 @@ void main()
 	cout << A[2] << endl;
 	//получение длины вектора
 	cout << "size= " << A() << endl;
-	Mnojestvo<long>C(10, 0);
+	Vector<long>C(10, 0);
 	//операция разности множеств
 	C = B - A;
 	cout << C << endl;
@@ -36,16 +36,16 @@ void main()
 	Money third;
 	third = first - second;
 	cout << "third: " << third;
-	Mnojestvo<Money> D(5, m);
+	Vector<Money> D(5, m);
 	cin >> D;
 	cout << D << endl;
-	Mnojestvo<Money> E(10, m);
+	Vector<Money> E(10, m);
 	cout << E << endl;
 	E = D;
 	cout << E << endl;
 	cout << D[2] << endl;
 	cout << "size=" << D() << endl;
-	Mnojestvo<Money> F(10, m);
+	Vector<Money> F(10, m);
 	F = D - E;
 	cout << F << endl;
 }
