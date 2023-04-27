0583e85674344299a8cdbdf290da7af7b2e8e011
User1807 <116075592+User1807@users.noreply.github.com>
2023-04-27 19:00:27 +0500

    Add files via upload

---
+++ OOP/Lab9/Lab9_main.cpp
@@ -0,0 +1,42 @@
+﻿#include "Vector.h"
+#include "Error.h"
+#include <iostream>
+using namespace std;
+int main()
+{
+	try
+	{
+		Vector z;
+		cout << "Size of vector?" << endl;
+		int size;
+		cin >> size;
+		Vector x(size);
+		cout << "Enter " << size << " elements of vector X" << endl;
+		cin >> x;
+		cout << "X: " << x;
+		cout << "Nomer?" << endl;
+		int i;
+		cin >> i;
+		cout << x[i] << endl;
+		Vector y(size);
+		cout << "Enter " << size << " elements of vector Y" << endl;
+		cin >> y;
+		cout << "Y: " << y;
+		cout << "Nomer?" << endl;
+		int j;
+		cin >> j;
+		cout << y[j] << endl;
+		z = x -  y;
+		cout << "Z: " << z << endl;
+		while(true)
+		{
+			cout << "X: " << x << endl;
+			--x;
+		}
+	}
+	catch (error e)
+	{
+		e.what();
+	}
+	return 0;
+}
