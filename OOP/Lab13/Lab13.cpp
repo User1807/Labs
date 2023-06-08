743d6b2f238d4f537ab100dd70bb87178a4a4f63
User1807 <116075592+User1807@users.noreply.github.com>
2023-06-08 17:28:57 +0500

    Add files via upload

---
+++ OOP/Lab13/Lab13.cpp
@@ -0,0 +1,82 @@
+﻿#include "Money.h"
+#include "vector"
+#include <iostream>
+#include <algorithm>
+using namespace std;
+typedef vector<Money> TVector;
+//формирование вектора
+TVector make_vector(int n)
+{
+	Money a;
+	TVector v;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a;
+		v.push_back(a);
+	}
+	return v;
+}
+//печать вектора
+void print_vector(TVector v)
+{
+	for (int i = 0; i < v.size(); i++)
+		cout << v[i] << endl;
+	cout << endl;
+}
+
+Money srednee(TVector v)
+{
+	Money s = {0, 0};
+	for (int i = 0; i < v.size(); i++)
+		s = s + v[i];
+	int n = v.size();
+	return s / n;
+}
+
+typedef vector<Money> TVector;
+Money s;
+
+struct Greater_s //больше, чем s
+{
+	bool operator()(Money m)
+	{
+		if (m > s) return true; else return false;
+	}
+};
+
+void multi(Money& m)
+{
+	m = m * s;
+}
+
+void main()
+{
+	int n, pos;
+	cout << "N?";
+	cin >> n;
+	TVector v;
+	v = make_vector(n);
+	print_vector(v);
+	TVector::iterator i;
+	//поставили итератор i на минимальный элемент
+	i = min_element(v.begin(), v.end());
+	Money min = *(i);
+	cout << "min: " << min << endl;
+	cout << "pos?"; cin >> pos;
+	v[pos] = min;
+	cout << "ZAMENA" << endl;
+	print_vector(v);
+	s = srednee(v);
+	cout << "srednee: " << s << endl;
+	Money m = { 0, 0 };
+	replace_if(v.begin(), v.end(), Greater_s(), m);
+	cout << "UDALENIE" << endl;
+	print_vector(v);
+	cout << "UMNOJENIE" << endl;
+	i = max_element(v.begin(), v.end());
+	s = *i;
+	cout << "max: " << s << endl;
+	//для каждого элемента вектора вызывается функция del
+	for_each(v.begin(), v.end(), multi);
+	print_vector(v);
+}
---
+++ OOP/Lab13/Money.cpp
@@ -0,0 +1,95 @@
+﻿#include "Money.h"
+#include <iostream>
+using namespace std;
+
+Money& Money::operator=(const Money& m)
+{
+	if (&m == this) return *this;
+	rubles = m.rubles;
+	pennies = m.pennies;
+	return *this;
+}
+
+Money& Money::operator--()
+{
+	int temp = rubles * 100 + pennies;
+	temp--;
+	rubles = temp / 100;
+	pennies = temp % 100;
+	return *this;
+}
+
+Money Money::operator --(int)
+{
+	int temp = rubles * 100 + pennies;
+	temp--;
+	Money t(rubles, pennies);
+	rubles = temp / 100;
+	pennies = temp % 100;
+	return t;
+}
+
+bool Money::operator==(const Money& t)
+{
+	int temp1 = rubles * 100 + pennies;
+	int temp2 = t.rubles * 100 + t.pennies;
+	bool compare = (temp1 == temp2);
+	return compare;
+}
+
+bool Money::operator!=(const Money& t)
+{
+	int temp1 = rubles * 100 + pennies;
+	int temp2 = t.rubles * 100 + t.pennies;
+	bool compare = (temp1 != temp2);
+	return compare;
+}
+
+istream& operator>>(istream& in, Money& t)
+{
+	cout << "rubles? "; in >> t.rubles;
+	cout << "pennies? "; in >> t.pennies;
+	return in;
+}
+ostream& operator<<(ostream& out, const Money& t)
+{
+	return (out << t.rubles << "," << t.pennies);
+}
+bool Money::operator <(const Money& t)
+{
+	if (rubles < t.rubles)return true;
+	if (rubles == t.rubles && pennies < t.pennies)return true;
+	return false;
+}
+bool Money::operator >(const Money& t)
+{
+	if (rubles > t.rubles)return true;
+	if (rubles == t.rubles && pennies > t.pennies)return true;
+	return false;
+}
+Money Money::operator+(const Money& t)
+{
+	int temp1 = rubles * 100 + pennies;
+	int temp2 = t.rubles * 100 + t.pennies;
+	Money p;
+	p.rubles = (temp1 + temp2) / 100;
+	p.pennies = (temp1 + temp2) % 100;
+	return p;
+}
+Money Money::operator*(const Money& t)
+{
+	int temp1 = rubles * 100 + pennies;
+	int temp2 = t.rubles * 100 + t.pennies;
+	Money p;
+	p.rubles = (temp1 * temp2) / 10000;
+	p.pennies = (temp1 * temp2) % 10000;
+	return p;
+}
+Money Money::operator/(const int& t)
+{
+	int temp1 = rubles * 100 + pennies;
+	Money p;
+	p.rubles = (temp1 / t) / 100;
+	p.pennies = (temp1 / t) % 100;
+	return p;
+}
